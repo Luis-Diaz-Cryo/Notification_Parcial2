7460fc37675ab8f4f59bcfbb4dc143b0697da41d
sebastianpica <sebas.picas08@gmail.com>
2024-10-21 07:18:12 -0500

    Fix OrderShippedNotification

--- Notification_Parcial2/src/main/java/co/edu/unisabana/Notification_Parcial2/service/NotificationService.java
+++ Notification_Parcial2/src/main/java/co/edu/unisabana/Notification_Parcial2/service/NotificationService.java
@@ -1,6 +1,8 @@
 package co.edu.unisabana.Notification_Parcial2.service;
 
+import co.edu.unisabana.Notification_Parcial2.entity.Venta;
 import co.edu.unisabana.Notification_Parcial2.repository.CustomerRepository;
+import co.edu.unisabana.Notification_Parcial2.repository.VentaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -19,6 +21,9 @@ public class NotificationService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private VentaRepository ventaRepository;
 
     private static final Logger logger = Logger.getLogger(NotificationService.class.getName());
 
@@ -43,8 +48,9 @@ public class NotificationService {
     }
 
     public void sendOrderShippedNotification(String event) {
-        Integer id = extractCustomerId(event);
-        String email = getEmailByCustomerId(id);
+        Integer orderId = extractOrderId(event);
+        Integer customerId = getCustomerIdByOrderId(orderId);
+        String email = getEmailByCustomerId(customerId);
         String message = "Your order has been shipped! Tracking Number: ";
 
         sendEmail(email, "Order Shipped", message);
@@ -71,7 +77,26 @@ public class NotificationService {
         }
     }
 
+    private Integer extractOrderId(String event) {
+        try {
+            JsonNode jsonNode = objectMapper.readTree(event);
+            return jsonNode.get("orderId").asInt();
+        } catch (Exception e) {
+            System.err.println("Failed to extract orderId from event: " + event);
+            System.err.println("Error details: " + e.getMessage());
+            return null;
+        }
+    }
 
+
+    private Integer getCustomerIdByOrderId(Integer orderId) {
+        if (orderId == null) {
+            System.err.println("CustomerId is null, cannot retrieve email.");
+            return null; // o maneja el caso según lo necesites
+        }
+        Venta venta = ventaRepository.findById(orderId).get();
+        return venta.getCliente().getId();
+    }
 
     private String getEmailByCustomerId(Integer customerId) {
         if (customerId == null) {
